d760be6e2c8aff788dde8ac0592cc7497a801793
LEEBONGHAK <dlqhdgkr456@gmail.com>
2023-06-03 18:22:31 +0900

    class and object 1-3

--- src/chapter5/classpart/Student.java
+++ src/chapter5/classpart/Student.java
@@ -6,11 +6,27 @@ public class Student {
     int grade;
     String address;
 
+
+    // 기본 생성자
+    // 생성자가 없는 경우 Java 컴파일러가 컴파일하기 전(pre-compile 단계)에 기본 생성자를 넣어준다
+    public Student() {}
+
+    // 오버로팅(overloading)
+    // 메소드/생성자 이름은 같지만 매개변수가 다른 경우 서로 다른 메소드/생성자로 인식됨
+    public Student(int id, String name) {
+        studentID = id;
+        studentName = name;
+    }
+
     public void showStudentInfo() {
         System.out.println(studentName + ", " + address);
     }
 
     public String getStudentName() {
         return studentName;
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
     }
 }
--- src/chapter5/classpart/StudentTest.java
+++ src/chapter5/classpart/StudentTest.java
@@ -2,10 +2,26 @@ package chapter5.classpart;
 
 public class StudentTest {
     public static void main(String[] args) {
+
+        // 인스턴스 생성
+        // 인스턴스는 힙(Heap) 메모리에 생성되며, 각각의 인스턴스는 다른 메모리에 다른 값을 가짐
+        // 할당된 메모리는 가비지 컬렉테(Garbage Collector)에 의해 자동으로 해제됨
         Student studentLee = new Student();
         studentLee.studentName = "이봉학";
-        studentLee.address = "hello world";
+        studentLee.studentID = 100;
+        studentLee.address = "hello";
+
+        Student studentKim = new Student(1, "tmp2");
+        studentKim.studentName = "김유신";
+        studentKim.studentID = 101;
+        studentKim.address = "world";
 
         studentLee.showStudentInfo();
+        studentKim.showStudentInfo();
+
+        // 인스턴스 출석 형식 `클래스의 전체 이름@주소값`
+        // 주소 값은 16진수로 이루어져 있으며 32 bit 주소값을 가진다.
+        System.out.println(studentKim);
+        System.out.println(studentLee);
     }
 }

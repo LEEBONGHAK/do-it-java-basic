259bc7894e2d4c2f4c11dba1f609ea42c0708fa3
LEEBONGHAK <dlqhdgkr456@gmail.com>
2023-06-04 01:04:14 +0900

    multi-dimensional array 3

---
+++ src/chapter7/src/array/TwoDimensionalArray.java
@@ -0,0 +1,21 @@
+package chapter7.src.array;
+
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        int[][] arr1 = new int[2][3];
+
+        // 행을 기준으로 length 를 가지고 온다
+        System.out.println(arr1.length);
+        System.out.println("===========");
+
+        int[][] arr2 = {{1,2,3}, {4,5,6}};
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                System.out.println(arr2[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("===========");
+
+    }
+}
